26466d905b2b681681991dc06f723b958002eecd
ruriga002 <eruriga002@gmail.com>
2026-05-04 08:28:31 +0300

    fix

--- src/__tests__/indexTest.js
+++ src/__tests__/indexTest.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
 import App from '../App'
-import { sampleProducts } from '../data'
+import { sampleProducts } from '../data.js'
 import '@testing-library/jest-dom'
 
 test('toggles dark mode on button click', () => {
